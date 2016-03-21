2df6445b88088409c6a6c04f8fbf988f10df48f5
litao <litaowork@aliyun.com>
2016-03-21 10:32:13 +0800

    add memberlist.vue file

--- src/components/admin/index.vue
+++ src/components/admin/index.vue
@@ -5,18 +5,14 @@
 		<search></search>
 		<!--再将父组件的数据传递给子组件，这里是传递给会员列表-->
 		<div class="row">
-			<div class="member" v-for="member in memberList">
-				<h3>{{member.userName}}</h3>
-				<p>{{member.userRank}}</p>
-			</div>
-			<div class="clear"></div>
+			<member v-bind:member-list="memberList"></member>
 		</div>
 	</div>
-
 </template>
 
 <script>
 	import search from './search.vue'
+	import member from './memberlist.vue'
 	//如果要求组件可复用，因为不同的search提交的url不同，所以需要可以配置url
 	export default {
 		name: 'index',
@@ -26,7 +22,8 @@
 			}
 		},
 		components: {
-			search
+			search,
+			member
 		},
 		events: {
 			getMember: function(members){
@@ -36,16 +33,3 @@
 	}
 </script>
 
-<style>
-	.clear{
-		clear:both;
-	}
-	.member{
-		border:1px solid #eee;
-		border-radius:5px;
-		padding:10px;
-		margin:10px;
-		float:left;
-		width:33%;
-	}
-</style>
---
+++ src/components/admin/memberlist.vue
@@ -0,0 +1,40 @@
+<template>
+	<div class="member" v-for="member in memberList" v-show="memberList.length > 0">
+		<h3>{{member.userName}}</h3>
+		<p>{{member.userRank}}</p>
+		<input type="button" value="选择" class="form-control" v-on:click="choose(member)">
+	</div>
+	<div class="clear"></div>
+</template>
+
+<script>
+	export default {
+		name: 'member',
+		props: ['memberList'],
+		data(){
+			return {
+				memberList:[],
+				member:{}
+			}
+		},
+		methods: {
+			choose: function(obj) {
+				console.log(obj)
+			}
+		}
+	}
+</script>
+
+<style>
+	.clear{
+		clear:both;
+	}
+	.member{
+		border:1px solid #eee;
+		border-radius:5px;
+		padding:10px;
+		margin:10px;
+		float:left;
+		width:20%;
+	}
+</style>
